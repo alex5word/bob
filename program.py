aad43e348149b1032234cf76cc06386f21f76a51
Scaueriu Daniel <alex.scaueriu@gmail.com>
2025-11-29 13:41:13 +0200

    Buna

--- program.py
+++ program.py
@@ -1,14 +1,2 @@
 #Se citesc n numere. Sa se afiseze numerele palindrom.
-def palindrom(int x):
-    s=str(x)
-    if s==s[::-1]:
-        return True
-    else
-        return False
-n=int(input())
-x=[]
-for i in range (n):
-  x1= int(input())
-  x.append(x1)
-for i in range(n):
-    palindrom(x[i])
+print("Buna")
